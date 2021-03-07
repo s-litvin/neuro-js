d5d8a58228e28d5874eaeaae71b646f219083407
Sergey <circumflex@gmailc.com>
2021-03-07 17:47:11 +0200

    Visualisation added

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+let cnvs = document.getElementById("NeuroNet");
+ctx = cnvs.getContext('2d');
+
+cnvs.width  = 1000;
+cnvs.height = 900;
+
+
 // --(i1)--(n1)
 //       \/    \__(o1)
 //       /\    /
@@ -40,9 +47,11 @@ perceptron.updateNeuron('o1', outputNeuron1);
 perceptron.updateNeuron('o2', outputNeuron2);
 
 let i = 0;
-for (i; i < 100000; i++) {
+for (i; i < 10000; i++) {
     // Forward pass
     perceptron.forwardPass();
+
+    drawNet(perceptron);
 
     // Learning
     perceptron.backPropagation();
@@ -54,3 +63,57 @@ for (i; i < 100000; i++) {
 }
 
 console.log('#: ' + i, 'O1: ' + perceptron.getNeuron('o1').cell.getOutput(), 'O2: ' + perceptron.getNeuron('o2').cell.getOutput());
+
+function drawNet(perceptron) {
+
+    ctx.font = "14px Arial";
+    ctx.fillStyle   = "#ffffff";
+    ctx.fillRect(0, 0, 1000, 900);
+
+    let neuronPositions = {};
+
+    for (let i = 0; i < perceptron.layers.length; i++) {
+
+        let neurons = perceptron.getNeuronsByLayer(perceptron.layers[i]);
+
+        for (let j = 0; j < neurons.length; j++) {
+
+            let posX = 100 * i;
+            let posY = 100 * j;
+            let neuron = neurons[j];
+
+            neuronPositions[neuron.id] = [posX, posY];
+
+            let neuronSize = 30;
+
+            let rightLinksCount = 0;
+            if (neuron.links.length > 0) {
+                for (let k = 0; k < neuron.links.length; k++) {
+                    if (neuron.links[k].type === 'left') {
+                        ctx.beginPath();
+                        ctx.moveTo(posX, posY + neuronSize / 2);
+                        ctx.lineTo(neuronPositions[neuron.links[k].id][0] + neuronSize, neuronPositions[neuron.links[k].id][1] + neuronSize / 2);
+                        ctx.lineWidth = neuron.links[k].weight * 2;
+                        ctx.strokeStyle = '#5555ff';
+                        ctx.stroke();
+                    } else {
+                        rightLinksCount++;
+                    }
+                }
+            }
+
+            ctx.fillStyle = "#888888";
+            ctx.fillRect(posX, posY, neuronSize, neuronSize);
+
+            ctx.fillStyle = "#ffffff";
+            ctx.fillText(neuron.id, neuronSize / 5 + 100 * i, neuronSize / 1.5 + 100 * j);
+
+            if (rightLinksCount === 0) {
+                ctx.fillStyle = "grey";
+                ctx.fillText(neuron.cell.getOutput().toFixed(2), neuronSize * 1.3 + 100 * i, neuronSize / 2.3 + 100 * j);
+            }
+        }
+
+    }
+
+}
