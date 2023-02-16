69136d6f0697c41aa4287ce1435e264c7bc7b45d
Serhiy Lytvyn <litvin.xx@gmail.com>
2023-02-16 23:50:38 +0200

    Activation functions added

--- neuron.js
+++ neuron.js
@@ -1,6 +1,11 @@
 class Cell
 {
-	constructor(layer, isBias = false, isRecurrent = false) {
+	static RELU = 'relu';
+	static LEAKYRELU = 'leakyrelu';
+	static SIGMOID = 'sigmoid';
+	static TANH = 'tanh';
+
+	constructor(layer, isBias = false, isRecurrent = false, activation = Cell.SIGMOID) {
 		this.setInput(0);
 		this.setOutput(0);
 		this.setTargetOutput(null);
@@ -9,6 +14,7 @@ class Cell
 		this.layer = layer;
 		this.isBias = isBias;
 		this.isRecurrent = isRecurrent;
+		this.activation = activation;
 	}
 
 	setInput(input) {
@@ -55,12 +61,39 @@ class Cell
 		} else if (this.layer === 0) {
 			this.setOutput(inputSum);
 		} else {
-			this.setOutput(1 / (1 + Math.pow(2.718, -1 * inputSum)));
+			this.setOutput(this.calcActivation(inputSum));
 		}
 
-		this.derivative = this.getOutput() * (1 - this.getOutput());
+		this.derivative = this.calcDerivative();
 
 		return this.getOutput();
+	}
+
+	calcActivation(inputSum) {
+		switch (this.activation) {
+			case Cell.RELU:
+				return inputSum < 0 ? 0 : inputSum;
+			case Cell.LEAKYRELU:
+				return inputSum < 0 ? 0.1 * inputSum : inputSum;
+			case Cell.TANH:
+				return (Math.pow(2.718, input) - Math.pow(2.718, -1 * inputSum)) / (Math.pow(2.718, input) + Math.pow(2.718, -1 * inputSum));
+			case Cell.SIGMOID:
+			default:
+				return 1 / (1 + Math.pow(2.718, -1 * inputSum));
+		}
+	}
+
+	calcDerivative() {
+		switch (this.activation) {
+			case Cell.RELU:
+			case Cell.LEAKYRELU:
+				return this.getOutput() < 0 ? 0 : 1;
+			case Cell.TANH:
+				return 1 - Math.pow(this.getOutput(), 2);
+			case Cell.SIGMOID:
+			default:
+				return this.getOutput() * (1 - this.getOutput());
+		}
 	}
 
 }
